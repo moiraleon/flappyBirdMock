304317768f09f374866fb0f55860101657a80954
Moira Leon <moira.leon2@gmail.com>
2022-02-10 22:18:27 -0700

    adding gravity function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var block = document.getElementById("block");
-var block = document.getElementById("hole");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
 
 hole.addEventListener('animationiteration', ()=>{
     var random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
 })
+
+setInterval(function(){
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (characterTop+3)+ "px";
+},10);

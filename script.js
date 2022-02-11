687707b1715da1b92c63ac6bd112b2dcccd43dea
Moira Leon <moira.leon2@gmail.com>
2022-02-10 23:03:22 -0700

    debugging

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
 var jumping = 0;
+var counter = 0;
 
 hole.addEventListener('animationiteration', ()=>{
     var random = -((Math.random()*300)+150);
@@ -10,20 +11,18 @@ hole.addEventListener('animationiteration', ()=>{
 })
 
 setInterval(function(){
-    var characterTop =
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
-    character.style.top = (characterTop+3)+ "px";
+    character.style.top = (characterTop+3) + "px";
     }
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    var cTop = -(500-characterTop)
-    if ((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop + 130)))){
-        alert("Game Over. Score: "+counter);
+    var cTop = -(500-characterTop);
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop + 130)))){
+        alert("Game Over. Score: "+(counter-1));
         character.style.top = 100 + "px";
         counter = 0;
     }
-
 },10);
 
 function jump(){

88fb1e38d5ab6506f35eec2eae70e66be1cffc92
Moira Leon <moira.leon2@gmail.com>
2022-02-10 22:37:37 -0700

    adding jump function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', ()=>{
     var random = -((Math.random()*300)+150);
@@ -10,5 +11,41 @@ hole.addEventListener('animationiteration', ()=>{
 setInterval(function(){
     var characterTop =
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
     character.style.top = (characterTop+3)+ "px";
+    }
 },10);
+
+// function jump(){
+//     jumping = 1;
+//     let jumpCount = 0;
+//     var jumpInterval = setInterval(function(){
+//         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//         if((characterTop>6)&&(counter<15)){
+//         character.style.top = (characterTop-5)+ "px";
+//         }
+//         if(jumpCount>20){
+//             clearInterval(jumpInterval)
+//             jumping=0;
+//             jumpCount = 0;
+//         }
+//         jumpCount ++;
+//     },10);
+// }
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+}

0614346d30fc65f1d1a5cb867d90db57516f97b5
Moira Leon <moira.leon2@gmail.com>
2022-02-10 22:45:58 -0700

    adding game feedback and score results

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var jumping = 0;
 hole.addEventListener('animationiteration', ()=>{
     var random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
+    counter++;
 })
 
 setInterval(function(){
@@ -14,24 +15,13 @@ setInterval(function(){
     if(jumping==0){
     character.style.top = (characterTop+3)+ "px";
     }
-},10);
+    if (characterTop>480){
+        alert("Game Over. Score: "+counter);
+        character.style.top = 100 + "px";
+        counter = 0;
+    }
 
-// function jump(){
-//     jumping = 1;
-//     let jumpCount = 0;
-//     var jumpInterval = setInterval(function(){
-//         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-//         if((characterTop>6)&&(counter<15)){
-//         character.style.top = (characterTop-5)+ "px";
-//         }
-//         if(jumpCount>20){
-//             clearInterval(jumpInterval)
-//             jumping=0;
-//             jumpCount = 0;
-//         }
-//         jumpCount ++;
-//     },10);
-// }
+},10);
 
 function jump(){
     jumping = 1;
